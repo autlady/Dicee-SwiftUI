e9b0d896b557933af32b49622c463d493335aa6a
Julia Grigoreva <julia_panina@mail.ru>
2022-05-19 16:42:08 +0300

    Добавлен .gitigonore. Добавлены файлы в assets. Сделана верстка экрана с использованием ZStack, HStack и VStack. Добавлена action для кнопки: изображения кубиков изменяются рандомно

---
+++ DiceeSwiftUI/DiceeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeSwiftUIApp.swift
+//  DiceeSwiftUI
+//
+//  Created by  Юлия Григорьева on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct DiceeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
